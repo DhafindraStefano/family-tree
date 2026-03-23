7653a218ffa751d7c3cde7061f541cd0412c40bf
DhafindraStefano <Dhafindra.razaqa@gmail.com>
2026-03-23 17:05:20 +0700

    API key fix lmao

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBF5pODzN22dyuMb8PfNpTbLFHhb80iD6U",
-  authDomain: "silsilah-keluarga-ikadam.firebaseapp.com",
-  projectId: "silsilah-keluarga-ikadam",
-  storageBucket: "silsilah-keluarga-ikadam.firebasestorage.app",
-  messagingSenderId: "654304723577",
-  appId: "1:654304723577:web:3995fafff87f95d3b3caa8",
-  measurementId: "G-W8Q61393H2",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Avoid re-initialising when Next.js hot-reloads
